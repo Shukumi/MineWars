0756691f993e261087c38ea5e7081393fe52934c
Shukumi <43240998+Shukumi@users.noreply.github.com>
2022-05-28 19:12:12 +0200

    Add: Test-Example

---
+++ JavaScript/main.js
@@ -0,0 +1,13 @@
+function testFunction() {
+	if(document.getElementById("test").style.color == "blue")
+		document.getElementById("test").style.color = "red";
+	else
+		document.getElementById("test").style.color = "blue";
+
+	}
+	
+	function test2Function() {
+
+		document.getElementById("test").style.color = "black";
+
+	}
